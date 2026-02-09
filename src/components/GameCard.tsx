eeab5b02b5f88ec2ac83bf8878dc1c64dd1805a9
Chandu <chandugarapati789@gmail.com>
2026-02-09 13:17:17 +0530

    Created a generic data fetching hook

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,6 @@ interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
-    
   return (
     <Card width="100%" overflow="hidden" borderRadius={10}>
       <Image
@@ -23,7 +22,9 @@ const GameCard = ({ game }: Props) => {
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent={"space-between"}>
-          <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
           <CriticScore score={game.metacritic} />
         </HStack>
       </CardBody>
--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -1,14 +1,18 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { Box } from "@chakra-ui/react";
+import React from "react";
 interface Props {
   children: React.ReactNode;
 }
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box overflow="hidden" width={{ base: "100%", md: "300px" }} borderRadius={10}>
+    <Box
+      overflow="hidden"
+      width={{ base: "100%", md: "300px" }}
+      borderRadius={10}
+    >
       {children}
     </Box>
-  )
-}
+  );
+};
 
-export default GameCardContainer
+export default GameCardContainer;
